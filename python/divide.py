4365ea29a5609837d2bbac905223ce257e4a9e71
Taru Rustagi <taru0904@umd.edu>
2021-06-03 04:55:46 -0400

    dataset from video

--- python/divide.py
+++ python/divide.py
@@ -20,19 +20,19 @@ file_train = open(args.keyword + '-train.txt', 'w')
 file_test = open(args.keyword + '-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     if counter == index_test:
         counter = 1
-        file_test.write(g_colab_full_path + "/" + title + '.jpg' + "\n")
+        file_test.write(g_colab_full_path + "/" + title + '.png' + "\n")
     else:
-        file_train.write(g_colab_full_path + "/" + title + '.jpg' + "\n")
+        file_train.write(g_colab_full_path + "/" + title + '.png' + "\n")
         counter = counter + 1
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.JPG")):  
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.PNG")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     if counter % index_test == 0:
         counter = 1
-        file_test.write(g_colab_full_path + "/" + title + '.JPG' + "\n")
+        file_test.write(g_colab_full_path + "/" + title + '.PNG' + "\n")
     else:
-        file_train.write(g_colab_full_path + "/" + title + '.JPG' + "\n")
+        file_train.write(g_colab_full_path + "/" + title + '.PNG' + "\n")
         counter = counter + 1
